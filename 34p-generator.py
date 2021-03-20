62c360106308a0cd6fdc958cf9e7e20206da5d41
kukaro <justkukaro@naver.com>
2021-03-20 14:51:48 +0900

    refactor: 34p-gen

--- 34p-generator.py
+++ 34p-generator.py
@@ -4,8 +4,8 @@ s = 'c('
 END = 100
 
 for i in range(END):
-    s += '"' + random.choice(['A', 'B', 'AB', 'O'])+'"'
-    if i != END-1:
+    s += '"' + random.choice(['A', 'B', 'AB', 'O']) + '"'
+    if i != END - 1:
         s += ','
 s += ")"
 print(s)
