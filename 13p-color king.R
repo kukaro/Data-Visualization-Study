042d5bf16f4e007f4d1a12ed6f847c12dfd05781
kukaro <justkukaro@naver.com>
2021-04-02 01:19:51 +0900

    fix: add utf8 file

--- 13p-color king.R
+++ 13p-color king.R
@@ -3,22 +3,23 @@
 # Created by: justk
 # Created on: 2021-03-19
 
-kings <- read.table('chosun kings.txt', header = T)
+kings <- read.table('chosun kings_utf8.txt', header = T)
 str(kings) #R객체의 내부 구조를 보여준다
 attach(kings)
 windows(width = 5.5, height = 4.5)
+#Only available on Windows.
 hist(Period,
      xlim = c(0, 70),
      ylim = c(0, 10),
      nclass = 14,
      right = F,
-     main = "\uC870\uC120\uC655\uC870",#조선왕조
-     xlab = "\uC7AC\uC704\uAE30\uAC04\u0028\uB144\u0029",#재위기간(년)
-     ylab = "\uBE48\uB3C4",#빈도
+     main = "\uC870\uC120\uC655\uC870", #조선왕조
+     xlab = "\uC7AC\uC704\uAE30\uAC04\u0028\uB144\u0029", #재위기간(년)
+     ylab = "\uBE48\uB3C4", #빈도
      col = c(
-         rep('lightblue', 2),
-         rep('royalblue', 6),
-         rep('navyblue', 6)
+       rep('lightblue', 2),
+       rep('royalblue', 6),
+       rep('navyblue', 6)
      )
 )
 #What is a rep function.
--- 34p-pie.R
+++ 34p-pie.R
@@ -6,7 +6,7 @@
 blood_type <- c("A", "O", "AB", "O", "B", "O", "O", "AB", "B", "B", "B", "A", "B", "O", "O", "A", "AB", "O", "A", "O", "B", "B", "O", "O", "B", "O", "O", "AB", "B", "O", "O", "A", "A", "AB", "AB", "AB", "B", "O", "B", "O", "AB", "B", "A", "A", "O", "O", "B", "B", "AB", "AB", "AB", "A", "A", "A", "O", "B", "A", "B", "O", "O", "AB", "O", "B", "AB", "O", "B", "A", "B", "B", "O", "O", "B", "AB", "AB", "A", "O", "AB", "A", "O", "O", "O", "AB", "O", "B", "O", "B", "AB", "B", "O", "A", "AB", "AB", "A", "A", "B", "O", "AB", "O", "B", "A")
 sorted_blood_type <- sort(table(blood_type), decreasing = T)
 par(mfrow = c(1, 2))
-slices<-c('red','blue','yellow','green')
-pie(sorted_blood_type,col=slices,radius=1,main="\uC6D0\uADF8\uB798\uD504")
+slices <- c('red', 'blue', 'yellow', 'green')
+pie(sorted_blood_type, col = slices, radius = 1, main = "\uC6D0\uADF8\uB798\uD504")
 print(sorted_blood_type)
 
