390b1487591892e2d18cc4f342fb10b64ebf41cd
kukaro <justkukaro@naver.com>
2021-04-10 01:13:44 +0900

    feat: adding comment as scatterplot explanation

--- 72p-scatterplot.R
+++ 72p-scatterplot.R
@@ -6,5 +6,5 @@
 exam <- read.table('./3ch/exam scores_2012.txt', header = T)
 str(exam) #str -> struct의 약자로 해당 객체를 자세히 보여준다
 attach(exam) #해당 데이터프레임 컬럼들을 변수로 접근할 수 있게 해준다.
-windows(height = 5.5, width = 5)
+windows(height = 5.5, width = 5) #이거 윈도우에서만 쓸 수 있다...
 plot(mid, final)
