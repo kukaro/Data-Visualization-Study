367f3498bdb7c6644f4c36cd045e4ff836b4a5f5
kukaro <justkukaro@naver.com>
2021-04-10 11:31:21 +0900

    refactor: changing parameter in windows and plot function

--- 79p-diamond.R
+++ 79p-diamond.R
@@ -7,14 +7,14 @@ library(ggplot2)
 data(diamonds) #diamonds 자료를 로딩함
 str(diamonds)
 attach(diamonds)
-windows(weight = 6, height = 6)
-plot(caret,
+windows(width = 6, height = 6)
+plot(carat,
      price,
      main = "diamonds",
      xlim = c(-0.5, 5.5),
      ylim = c(-1000, 21000),)
-windows(weight = 6, height = 6)
-plot(caret,
+windows(width = 6, height = 6)
+plot(carat,
      sqrt(price),
      main = "diamonds",
      xlim = c(-0.5, 5.5),
