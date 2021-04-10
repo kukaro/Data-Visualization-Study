166ef85d8dbd052fbda12a73cf0713e0cba2e134
kukaro <justkukaro@naver.com>
2021-04-10 11:48:03 +0900

    feat: adding haxgonal binning

---
+++ 80p-hexagonal-binning.R
@@ -0,0 +1,19 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: coeat
+# Created on: 2021/04/10
+
+install.packages('hexbin')
+library(hexbin)
+windows(height = 7, width = 6.4)
+hexbinplot(
+  sqrt(price) ~ carat,
+  data = diamonds,
+  main = "diamonds",
+  xlim = c(-0.5, 5.5),
+  ylim = c(0, 160),
+  xbins = 25,
+  aspect = 1,
+  colorkey = F,
+  colramp = function(n) magenta(n, 255, 255)
+)
