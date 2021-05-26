7c636ac6b6080653561a8205ce4b4c61b8fb57c6
kukaro <justkukaro@naver.com>
2021-05-27 00:57:09 +0900

    feat:adding point

--- finalexam/n4.R
+++ finalexam/n4.R
@@ -4,7 +4,19 @@
 # Created on: 2021/05/26
 
 library(coronavirus)
+library(ggplot2)
 cor <- coronavirus
-# cor$country=='Korea, South'
 contry_type <- cor[, c(3, 6)]
 cor_kor <- cor[contry_type$country == 'Korea, South' & contry_type$type == 'confirmed',]
+
+wrld <- map_data('world')
+korea <- wrld[wrld$region %in% 'South Korea',]
+
+qplot(
+  long,
+  lat,
+  data = korea,
+  geom = 'polygon',
+  fill = region,
+  group = group
+)+geom_point(aes(long,lat,fill=NULL,group=NULL),size=1,data=cor_kor)
